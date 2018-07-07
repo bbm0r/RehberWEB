8e6caea9dfa5f6caa67c34148827752d7c275391
bogac <bogac@DESKTOP-05IMPEI>
2018-07-07 14:29:47 +0300

    Giris session bean eklendi

--- RehberWEB/src/main/java/com/bilisimegitim/rehberweb/mb/GirisMB.java
+++ RehberWEB/src/main/java/com/bilisimegitim/rehberweb/mb/GirisMB.java
@@ -1,14 +1,65 @@
 
 package com.bilisimegitim.rehberweb.mb;
 
+import com.bilisimegitim.rehberweb.session.GirisFacade;
+import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.facelets.FaceletCache;
 
 @Named(value = "girisMB")
 @RequestScoped
 public class GirisMB {
 
+    @EJB
+    private GirisFacade girisFacade;
+
+   private String kullanici;
+   private String sifre;
+   
+    
+    
+    
+    
     public GirisMB() {
     }
+
     
-}
+    public String getKullanici() {
+        return kullanici;
+    }
+
+    public void setKullanici(String kullanici) {
+        this.kullanici = kullanici;
+    }
+
+    public String getSifre() {
+        return sifre;
+    }
+
+    public void setSifre(String sifre) {
+        this.sifre = sifre;
+    }
+    
+    public String giris()
+    {
+       boolean sonuc = girisFacade.girisKontrol(kullanici, sifre);
+    
+    if(sonuc)
+    {
+    return "menu.xhtml";
+    }
+else
+    {
+        FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR, "hata", "kullanıcı adı ya da şifreyi yanlış girmişin"));
+        return "";
+       
+               
+    }  
+    
+    
+    
+    
+    }
