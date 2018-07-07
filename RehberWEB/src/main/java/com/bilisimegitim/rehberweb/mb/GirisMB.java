80966f114e9f1da37bbac33b44b324664b2b58a4
bogac <bogac@DESKTOP-05IMPEI>
2018-07-07 14:15:35 +0300

    Giris session bean eklendi

---
+++ RehberWEB/src/main/java/com/bilisimegitim/rehberweb/mb/GirisMB.java
@@ -0,0 +1,14 @@
+
+package com.bilisimegitim.rehberweb.mb;
+
+import javax.inject.Named;
+import javax.enterprise.context.RequestScoped;
+
+@Named(value = "girisMB")
+@RequestScoped
+public class GirisMB {
+
+    public GirisMB() {
+    }
+    
+}
